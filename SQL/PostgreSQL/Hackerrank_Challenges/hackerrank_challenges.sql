be30edc7f469bd5e319101ecd507217255ca7fd5
NandishRPatel <1418beit30031@gmail.com>
2023-05-17 04:17:06 +1000

    Ollivander's Inventory added

--- SQL/PostgreSQL/Hackerrank_Challenges/hackerrank_challenges.sql
+++ SQL/PostgreSQL/Hackerrank_Challenges/hackerrank_challenges.sql
@@ -194,3 +194,33 @@ FROM (SELECT num FROM decrement_cte) AS subq;
 
 SELECT REPEAT('*',subq.cnt)
 FROM (SELECT generate_series(5, 1, -1) AS cnt) AS subq;
+
+
+## 7. Ollivander's Inventory
+
+/*
+URL - https://www.hackerrank.com/challenges/harry-potter-and-wands/problem
+
+Harry Potter and his friends are at Ollivander's with Ron, finally replacing Charlie's old broken wand.
+
+Hermione decides the best way to choose is by determining the minimum 
+number of gold galleons needed to buy each non-evil wand of high power 
+and age. Write a query to print the id, age, coins_needed, and power 
+of the wands that Ron's interested in, sorted in order of descending 
+power. If more than one wand has same power, sort the result in order 
+of descending age.
+*/
+
+SELECT w.id, wp.age, w.coins_needed, w.power 
+FROM wands w
+JOIN wands_property wp
+    ON w.code = wp.code
+WHERE wp.is_evil = 0 AND w.coins_needed = 
+(
+    SELECT MIN(w1.coins_needed)
+    FROM wands w1
+    JOIN wands_property wp1
+    ON w1.code = wp1.code
+    WHERE wp.age = wp1.age AND w.power = w1.power
+)
+ORDER BY power DESC, age DESC
