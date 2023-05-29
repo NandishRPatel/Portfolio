d109bd1d76bb8a140e90a57be5341005350407a8
NandishRPatel <1418beit30031@gmail.com>
2023-05-30 04:00:32 +1000

    15 days of learning SQL

--- SQL/PostgreSQL/Hackerrank_Challenges/hackerrank_challenges.sql
+++ SQL/PostgreSQL/Hackerrank_Challenges/hackerrank_challenges.sql
@@ -492,3 +492,54 @@ JOIN (
     ON subq2.contest_id = cts.contest_id
 GROUP BY cts.contest_id, cts.hacker_id, cts.name
 HAVING subq1.sum_total_submissions + subq2.sum_total_views > 0;
+
+
+## 14. 15 Days of Learning SQL
+
+/*
+URL - https://www.hackerrank.com/challenges/15-days-of-learning-sql/problem
+
+Julia conducted a  days of learning SQL contest. The start date of the contest was March 01, 2016 and the 
+end date was March 15, 2016.
+
+Write a query to print total number of unique hackers who made at least  submission each day (starting on 
+the first day of the contest), and find the hacker_id and name of the hacker who made maximum number of 
+submissions each day. If more than one such hacker has a maximum number of submissions, print the lowest 
+hacker_id. The query should print this information for each day of the contest, sorted by the date.
+*/
+
+
+WITH submission_counts AS (
+    SELECT submission_date, hacker_id, count(*) AS num_submissions
+    FROM submissions
+    GROUP BY submission_date, hacker_id
+),
+top_hacker_rank AS (
+    SELECT submission_date, hacker_id, rank() OVER (PARTITION BY submission_date ORDER BY num_submissions DESC, hacker_id ) AS rank_top_hacker
+    FROM submission_counts
+),
+min_max_dates AS (
+    SELECT MIN(submission_date) AS min_date, MAX(submission_date) AS max_date
+    FROM submissions
+),
+hackers_with_one_submission_each_day AS (
+    SELECT submission_date, hacker_id, count(S.submission_id) AS num_submissions
+    FROM submissions S
+    CROSS JOIN min_max_dates M
+    WHERE EXTRACT(DAY FROM submission_date - M.min_date) = 
+        (SELECT COUNT(DISTINCT S2.submission_date)
+        FROM submissions S2 
+        WHERE S.hacker_id = S2.hacker_id AND S2.submission_date < S.submission_date)
+    GROUP BY submission_date, hacker_id
+),
+unique_hacker_counts_each_day AS (
+    SELECT submission_date, count(DISTINCT hacker_id) AS num_unique_hackers
+    FROM hackers_with_one_submission_each_day
+    GROUP BY submission_date
+)
+SELECT U.submission_date, U.num_unique_hackers, T.hacker_id, H.name
+FROM unique_hacker_counts_each_day U      
+INNER JOIN top_hacker_rank T ON U.submission_date = T.submission_date 
+INNER JOIN hackers H ON T.hacker_id = H.hacker_id
+WHERE T.rank_top_hacker = 1
+ORDER BY U.submission_date;
